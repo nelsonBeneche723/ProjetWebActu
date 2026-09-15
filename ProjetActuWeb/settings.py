abde123aad729cd056a0e03d4d2b1d8458ca5db0
Nelson Beneche <rivalsonj@gmail.com>
2026-09-15 19:56:11 -0400

    Update settings.py

--- ProjetActuWeb/settings.py
+++ ProjetActuWeb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-5#i&t^6@l%)psvjzr^w@f!ipea^p4gkh*nx$*f4#otnjp9mdmk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.0.176','192.168.0.175', 'onRender','*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.0.176','192.168.0.175', 'projetactuweb-3.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
 
