4ab35852012b503d0899fa9f6e27d52ae0896bd8
Nelson Beneche <rivalsonj@gmail.com>
2026-09-16 19:43:30 -0400

    Update settings.py

--- ProjetActuWeb/settings.py
+++ ProjetActuWeb/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
