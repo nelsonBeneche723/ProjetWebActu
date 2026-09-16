2b38becde0042f7cc9c49d009cc3b56f90df3bd8
Nelson Beneche <rivalsonj@gmail.com>
2026-09-15 23:05:26 -0400

    Update settings.py

--- ProjetActuWeb/settings.py
+++ ProjetActuWeb/settings.py
@@ -145,15 +145,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 
 # Default primary key field type
