07e4aec166fffacd5e515cf9309e293835a029e5
Nelson Beneche <rivalsonj@gmail.com>
2026-09-20 11:32:51 -0400

    Update settings.py

--- ProjetActuWeb/settings.py
+++ ProjetActuWeb/settings.py
@@ -100,8 +100,7 @@ WSGI_APPLICATION = 'ProjetActuWeb.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=os.environ.get("DB_SSL", "False") == "True",
+        conn_max_age=600
     )
 }
 
