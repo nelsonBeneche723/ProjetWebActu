68cd4f41b2a876dfdeb34ddbeaa651caaab443c5
Nelson Beneche <rivalsonj@gmail.com>
2026-09-20 11:29:47 -0400

    Update settings.py

--- ProjetActuWeb/settings.py
+++ ProjetActuWeb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
 from pathlib import Path
 import os
+import dj_database_url
 
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,13 +97,20 @@ WSGI_APPLICATION = 'ProjetActuWeb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=os.environ.get("DB_SSL", "False") == "True",
+    )
 }
+
+#DATABASES = {
+ #   'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+  #  }
+#}
 
 
 # Password validation
